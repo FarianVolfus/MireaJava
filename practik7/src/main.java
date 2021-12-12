a427cf9d46f7fecfea1f0d89df0e5951db01a707
FarianVolfus <91363976+FarianVolfus@users.noreply.github.com>
2021-12-12 20:03:27 +0300

    Add files via upload
    pr7

---
+++ practik7/src/main.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class main {
+    public static void main(String[] args) throws IOException {
+        stack stack = new stack();
+        queue onQueue = new queue();
+        dequene dequeue = new dequene();
+    }
+}
